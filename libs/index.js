c71e5a160259e186eabb7f3ad3600b9b263707aa
Pavel Filippov <zanziver@gmail.com>
2020-09-13 15:59:57 +0200

    Working with multiple users, (+) users removal, (+) looking up names in contact list

---
+++ libs/index.js
@@ -0,0 +1,76 @@
+async function openSharingVideoWindow (browser, page, log, URL_YOUTUBE_STUDIO_VIDEO) {
+  await page.goto(URL_YOUTUBE_STUDIO_VIDEO);
+  await page.waitFor(1000);
+
+  log.info('Open studio')
+
+  url = await page.url();
+
+  log.info('Curently on page ' + url);
+
+  await page.waitForSelector('ytcp-icon-button[id="overflow-menu-button"]');
+  await page.click('ytcp-icon-button[id="overflow-menu-button"]');
+  await page.waitFor(300);
+
+  const elements = await page.$x('//yt-formatted-string[text()="Share privately"]')
+
+  await elements[0].click();
+
+  const newPagePromise = new Promise(x => browser.once('targetcreated', target => x(target.page())));
+  const popup = await newPagePromise;
+
+  url = await popup.url();
+  log.info('Currently on page ', url);
+
+  await page.goto(url);
+  await page.bringToFront();
+  await page.waitFor(1000);
+}
+
+async function authorize (page, log, GOOGLE_USER, GOOGLE_PASSWORD) {
+  log.info('Need to authorize');
+  await page.waitForSelector('input[type="email"]')
+
+  await page.type('input[type="email"]', GOOGLE_USER);
+
+  await page.keyboard.press('Enter');
+  await page.waitFor(1000);
+
+  await page.waitForSelector('input[type="password"]')
+
+  await page.type('input[type="password"]', GOOGLE_PASSWORD);
+  await page.waitFor(1000);
+
+  log.info('Authorizing');
+
+  await page.keyboard.press('Enter');
+  await page.waitFor(1000);
+}
+
+async function getNamesFromContactList (page, log, emails, URL_GOOGLE_CONTACTS) {
+  let result = {};
+
+  for (let i = 0; i < emails.length; i++) {
+    let email = emails[i];
+    let emailName;
+    if (email!=='') {
+      await page.goto(URL_GOOGLE_CONTACTS + email);
+      await page.waitFor(1000);
+      log.info('Opening contacts for ' + email);
+      try {
+         emailName = await page.$eval(`span[role="button"]`, el => el.innerText);
+      }
+      catch(ex) { }
+      if (emailName) {
+        result[email] = emailName;
+        log.info(`There is a name ${emailName} for the ${email}`);
+      } else {
+        log.info(`There is no name for the ${email}`);
+      }      
+    }
+  }
+
+  return result;
+}
+
+module.exports = { openSharingVideoWindow, authorize, getNamesFromContactList }
--- share-youtube-video.js
+++ share-youtube-video.js
@@ -3,10 +3,16 @@ const path = require('path');
 
 const puppeteer = require('puppeteer-extra');
 
+const libs = require('./libs');
+const { openSharingVideoWindow, authorize, getNamesFromContactList } = libs;
+
 const log = require('simple-node-logger').createSimpleFileLogger('project.log');
 
 const config = require('./config.js');
-const { URL_GOOGLE_ACCOUNTS, URL_YOUTUBE_STUDIO_VIDEO, GOOGLE_USER, GOOGLE_PASSWORD, USER_TO_SHARE } = config;
+const { URL_GOOGLE_ACCOUNTS, URL_YOUTUBE_STUDIO_VIDEO, URL_GOOGLE_CONTACTS, GOOGLE_USER, GOOGLE_PASSWORD, USERS_TO_SHARE, USERS_TO_REMOVE } = config;
+
+const USERS_TO_SHARE_ARRAY  = USERS_TO_SHARE.split(',');
+const USERS_TO_REMOVE_ARRAY = USERS_TO_REMOVE.split(',');
 
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin());
@@ -22,35 +28,7 @@ fs.access(dataDir, fs.constants.W_OK, function(err) {
 
 log.info(`Using folder ${dataDir}`);
 
-async function openSharingVideoWindow (browser, page, log) {
-  await page.goto(URL_YOUTUBE_STUDIO_VIDEO);
-  await page.waitFor(1000);
-
-  log.info('Open studio')
-
-  url = await page.url();
-
-  log.info('Curently on page ' + url);
-
-  await page.waitForSelector('ytcp-icon-button[id="overflow-menu-button"]');
-  await page.click('ytcp-icon-button[id="overflow-menu-button"]');
-  await page.waitFor(300);
-
-  const elements = await page.$x('//yt-formatted-string[text()="Share privately"]')
-  await elements[0].click();
-
-  const newPagePromise = new Promise(x => browser.once('targetcreated', target => x(target.page())));
-  const popup = await newPagePromise;
-
-  url = await popup.url();
-  log.info('Currently on page ', url);
-
-  await page.goto(url);
-  await page.bringToFront();
-  await page.waitFor(1000);
-}
-
-puppeteer.launch({ headless: true, product: 'chrome', userDataDir: dataDir}).then(async browser => {
+puppeteer.launch({ headless: false, product: 'chrome', userDataDir: dataDir}).then(async browser => {
 
   const page = await browser.newPage();
 
@@ -58,9 +36,13 @@ puppeteer.launch({ headless: true, product: 'chrome', userDataDir: dataDir}).the
 
   await page.setViewport({ width: 1280, height: 800 })
 
+  await page.setBypassCSP(true);
+
+  let usersToNames = await getNamesFromContactList(page, log, [...USERS_TO_SHARE_ARRAY, ...USERS_TO_REMOVE_ARRAY], URL_GOOGLE_CONTACTS);
+
   await page.goto(URL_YOUTUBE_STUDIO_VIDEO);
 
-  log.info('Open studio');
+  log.info('Opening studio');
 
   await page.waitFor(3000);
 
@@ -69,43 +51,57 @@ puppeteer.launch({ headless: true, product: 'chrome', userDataDir: dataDir}).the
   log.info('Currently on page ' + url);
 
   if (url.indexOf('ServiceLogin')>0) {
+    await authorize(page, log, GOOGLE_USER, GOOGLE_PASSWORD);
+  }
 
-    log.info('Need to authorize');
+  await openSharingVideoWindow(browser, page, log, URL_YOUTUBE_STUDIO_VIDEO);
 
-    await page.waitForSelector('input[type="email"]')
+  await page.waitForSelector('textarea.metadata-share-contacts');
 
-    await page.type('input[type="email"]', GOOGLE_USER);
+  log.info(`Typing new users (${USERS_TO_SHARE_ARRAY.length})...`);
 
-    await page.keyboard.press('Enter');
-    await page.waitFor(1000);
+  for (let i = 0; i < USERS_TO_SHARE_ARRAY.length; i++) {
+    await page.type('textarea.metadata-share-contacts', USERS_TO_SHARE_ARRAY[i] + ',', {delay: 20});
+  }
 
-    await page.waitForSelector('input[type="password"]')
+  log.info(`Removing users (${USERS_TO_REMOVE_ARRAY.length})...`);
 
-    await page.type('input[type="password"]', GOOGLE_PASSWORD);
-    await page.waitFor(1000);
+  for (let i = 0; i < USERS_TO_REMOVE_ARRAY.length; i++) {
+    const email = USERS_TO_REMOVE_ARRAY[i];
+    const checkString = usersToNames[email] || email;
 
-    log.info('Authorizing');
+    try {
+      await page.click(`button[aria-label^="Delete ${checkString}"]`);
+      log.info(`User ${checkString} was successfully removed`);
+    }
+    catch (ex) {
+      log.info(`No user ${checkString} to remove`);
+    }
 
-    await page.keyboard.press('Enter');
     await page.waitFor(1000);
   }
 
-  openSharingVideoWindow(browser, page, log);
-
-  await page.waitForSelector('textarea.metadata-share-contacts');
-  await page.type('textarea.metadata-share-contacts', USER_TO_SHARE, {delay: 20});
   await page.click('button.sharing-dialog-ok');
   await page.waitFor(3000);
 
-  openSharingVideoWindow(browser, page, log);
+  log.info('Video was shared, now testing');
+
+  await openSharingVideoWindow(browser, page, log, URL_YOUTUBE_STUDIO_VIDEO);
   await page.waitForSelector('div.acl-target-list-inner-container');
 
-  if (await page.waitForSelector(`div[title="${USER_TO_SHARE}"]`, { timeout: 3000 }) === null) {
-    log.error(`Video is not shared with ${USER_TO_SHARE}, please try again`);
-  } else {
-    log.info(`All good, video is shared with ${USER_TO_SHARE}`);
-  }
+  for (let i = 0; i < USERS_TO_SHARE_ARRAY.length; i++) {
+    let email = USERS_TO_SHARE_ARRAY[i];
+    const checkString = usersToNames[email] || email;
+    try {
+      await page.waitForSelector(`div[title="${checkString}"]`, { timeout: 300 });
+      log.info(`Video is shared with ${checkString}`);
+    }
+    catch (ex) {
+      log.info(`Video is not shared with ${checkString}`);
+    }
+  };
 
-  await browser.close();
+ log.info('Closing session');
+ await browser.close();
 
 });
